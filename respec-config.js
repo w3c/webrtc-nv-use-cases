af4cba24b0f6243ce1b059eb083cd7577f5182ad
Bernard Aboba <bernard.aboba@gmail.com>
2018-06-29 11:52:21 -0700

    Formatting

--- respec-config.js
+++ respec-config.js
@@ -4,8 +4,7 @@ var respecConfig = {
   edDraftURI: "https://w3c.github.io/webrtc-nv-usecases/",
   shortName: "webrtc-nv-usecases",
   editors: [
-    { name: "Bernard Aboba", company: "Microsoft Corporation", w3cid: "65611" },
-    { name: "Peter Thatcher", company: "Google", w3cid: "68236" }
+    { name: "Bernard Aboba", company: "Microsoft Corporation", w3cid: "65611" }
   ],
   authors: [
   ],
